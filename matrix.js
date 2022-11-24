fe29f26a75d7867f16ac828aaf241c8a523d9023
miguel celma <mrcelma3@gmail.com>
2022-11-24 19:33:45 +0100

    playing around

--- matrix.js
+++ matrix.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('canvas');
-const    ctx = canvas.getContext("2d");
+const    ctx = canvas.getContext('2d');
 var cw = window.innerWidth,
     ch = window.innerHeight,
     charArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
